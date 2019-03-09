51263277714d0a9d7e33e6ee327672c14329d075
ashoka1234 <ashoka1234@hotmail.com>
2019-03-10 08:46:09 +1100

    Added setup, travis CI

--- setup.py
+++ setup.py
@@ -1,7 +1,37 @@
 from setuptools import setup, find_packages
 
+tests_require = [
+    'pytest'
+]
+
+extras_require = {
+    'test' : tests_require
+}
+
 setup(
-    name="ashoka_challenge",
-    version="0.1",
+
+    name='ashoka_challenge',
+
+    version='0.1',
+
+    python_requires='>=3.6',
+
+    author='Ashoka Jayawardena',
+
     packages=find_packages(),
+
+    install_requires=[
+        'click',
+        'flask',
+        'pymongo'
+    ],
+
+    extras_require=extras_require,
+    tests_require=tests_require,
+
+    entry_points={
+        'console_scripts': [
+            'challenge-manage = manage:cli',
+        ],
+    }
 )
